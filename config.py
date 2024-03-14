4d4e408aa5b593da6a8c09475a5bc5e3c79b5ffd
RahulSJb <161461605+RahulSJb@users.noreply.github.com>
2024-03-14 22:18:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/stkeditz/cheems-God",
+    "https://github.com/Rahulsjb/cheems-God",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
